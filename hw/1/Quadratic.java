e197e536b4fcdccb7020caf42e95ac6256b9f0d3
Juan Arias <juan.arias@marist.edu>
2022-02-05 15:09:08 -0500

    Feedback

--- hw/1/Quadratic.java
+++ hw/1/Quadratic.java
@@ -1,11 +1,11 @@
-public class quadratic{
+public class Quadratic{
 
     public static void main(String [] args){
 
         int a = 2;
         int b = 5;
         int c = 4;
-        int disc = b*b - 4 * a * c;
+        double disc = b*b - 4 * a * c;
         System.out.println(disc);
 
     }
--- hw/1/_100pattern.java
+++ hw/1/_100pattern.java
@@ -1,4 +1,5 @@
-public class  {
+// Class name can not start with a number
+public class _100pattern  {
 
     public static void main(String[] args) {
         System.out.println("* ***** *****");
