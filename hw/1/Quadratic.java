fa061cf0316d4612b64607e52d133a48d0fddc3f
NickPasta9 <89875993+NickPasta9@users.noreply.github.com>
2022-02-04 22:22:36 -0500

    Add files via upload

---
+++ hw/1/Quadratic.java
@@ -0,0 +1,12 @@
+public class quadratic{
+
+    public static void main(String [] args){
+
+        int a = 2;
+        int b = 5;
+        int c = 4;
+        int disc = b*b - 4 * a * c;
+        System.out.println(disc);
+
+    }
+}
---
+++ hw/1/acceleration.java
@@ -0,0 +1,10 @@
+public class acceleration {
+
+    public static void main(String[] args) {
+        double vel0 = 4.6;
+        double vel1 = 11.5;
+        double time = 0.6;
+        double acceleration = (vel1 - vel0) / time;
+        System.out.println(acceleration);
+    }
+}
