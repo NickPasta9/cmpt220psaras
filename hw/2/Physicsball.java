960eb062b7d18189cf99c25975fc15a46d8312d6
NickPasta9 <89875993+NickPasta9@users.noreply.github.com>
2022-02-11 16:06:50 -0500

    Add files via upload

---
+++ hw/2/Physicsball.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the ball travel time in seconds: ");
+        double t = input.nextDouble();
+        double d = (0 * t) + (9.8 * (t*t)) / 2;
+        System.out.println("The height of the building is " + d + " meters");
+
+    }
+}
