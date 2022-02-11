cc1fdd695af5d19881ab723e4bf308ebaf4aa854
NickPasta9 <89875993+NickPasta9@users.noreply.github.com>
2022-02-11 15:29:17 -0500

    Add files via upload

---
+++ hw/2/Circlediameter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+Public Class Circle Diameter{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius of a circle: ");
+        double radius = input.nextDouble();
+        double diameter = 2 * radius1;
+        double area = 3.14 * Math.pow(radius1,2);
+        System.out.println("The diameter is: " + diameter);
+        System.out.println("The area is: " + area);
+
+    }
+}
