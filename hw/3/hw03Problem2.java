3f0cc1d5bc6230b4929274f788171aa374e975eb
NickPasta9 <89875993+NickPasta9@users.noreply.github.com>
2022-02-18 13:36:11 -0500

    Add files via upload

---
+++ hw/3/hw03Problem2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Problem2{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a numerator: ");
+        double numerator = input.nextDouble();
+        System.out.print("Enter a denominator: ");
+        double denominator = input.nextDouble();
+
+        if (numerator < denominator || numerator == denominator){
+            System.out.println(numerator + " / " + denominator + " is a proper fraction.");
+        }
+        else if (numerator % denominator == 0){
+            
+            System.out.println(numerator + " / " + denominator + " is an improper fraction and it can be reduced to " + (numerator / denominator));
+        }
+        else if (numerator % denominator != 0){
+            System.out.println(numerator + " / " + denominator + " is an improper fraction and its mixed fraction is " + ((int)numerator / (int)denominator) + " + " + (numerator % denominator) + " / " + denominator);
+        }
+
+        input.close();
+
+
+
+    }
+}
