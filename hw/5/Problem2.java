c2d358ef5d9147972afb83486f8de0b1b57985bb
NickPasta9 <89875993+NickPasta9@users.noreply.github.com>
2022-03-04 18:27:47 -0500

    Add files via upload

---
+++ hw/5/Problem2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Problem2 {
+    
+    public static String decimalToBinary(int value) {
+
+        int[] binaryN = new int[11];
+        int count = 0;
+
+        while (value > 0) {
+            binaryN[count] = value % 2;
+            value = value / 2;
+            count++;
+        }
+        int[] reverse = new int[binaryN.length];
+        for (int i = 0, j = reverse.length - 1; i < binaryN.length; i++, j--)
+           reverse[j] = binaryN[i];
+        String binaryStr = Arrays.toString(reverse);
+        return binaryStr;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int n = input.nextInt();
+        input.close();
+        System.out.println("The binary value is "+decimalToBinary(n));
+    }
+}
